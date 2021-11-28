6125a661c0b491e54828c92742322599d17b1b20
mhrehbein <matheus.rehbein@philips.com>
2021-11-28 18:09:19 -0300

    ensure Login Route returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -18,5 +18,8 @@ module.exports = class LoginRouter {
       return HttpRespose.badRequest('password')
     }
     this.authUseCase.auth(email, password)
+    return {
+      statusCode: 401
+    }
   }
 }
