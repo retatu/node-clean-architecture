e30de7ba24809fa453cde3a2a63f3090a85fe791
mhrehbein <matheus.rehbein@philips.com>
2021-12-01 17:33:18 -0300

    ensure email validator returns true if validator returns true

---
+++ src/presentation/utils/email-validator.spec.js
@@ -0,0 +1,18 @@
+class EmailValidator {
+  isValid () {
+    return true
+  }
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+})
