1eb62127cc407b8492428f857dcb7ea730f97980
mhrehbein <matheus.rehbein@philips.com>
2021-12-01 17:44:33 -0300

    ensure email validator calls validator with correct params

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -18,7 +18,12 @@ describe('Email Validator', () => {
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const { sut } = makeSut()
-    const isEmailValid = sut.isValid('valid_email@mail.com')
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+  test('Should return call validator with correct email', () => {
+    const { sut } = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
