052cd41145cf8d9078f6d774953768168ad0618d
mhrehbein <matheus.rehbein@philips.com>
2021-12-01 18:39:24 -0300

    ensure AuthUseCase returns null if loadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -18,7 +18,10 @@ class AuthUseCase {
     if (!this.loadUserByEmailRepository.load) {
       throw new InvalidParamError('repository')
     }
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -62,5 +65,10 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@email.com', 'any_passowrd')
     expect(promise).rejects.toThrow(new InvalidParamError('repository'))
+  })
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@email.com', 'any_passowrd')
+    expect(accessToken).toBe(null)
   })
 })
