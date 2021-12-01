c83b8af33e7782be32bea312758e6b05a3c2dcc9
mhrehbein <matheus.rehbein@philips.com>
2021-12-01 18:11:27 -0300

    ensure authusecase throws missing param error if no email is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -3,7 +3,7 @@ const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
   async auth (email) {
     if (!email) {
-      throw new MissingParamError()
+      throw new MissingParamError('email')
     }
   }
 }
@@ -12,6 +12,6 @@ describe('Auth UseCase', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
