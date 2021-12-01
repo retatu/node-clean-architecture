0612a38345a52a9b5f08d7e08c7bc2f99d2a3f79
mhrehbein <matheus.rehbein@philips.com>
2021-12-01 17:29:42 -0300

    move errors to custom folder

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,7 +1,4 @@
-const InvalidParamError = require('../helpers/invalid-param-error')
-const MissingParamError = require('../helpers/missing-param-error')
-const ServerError = require('../helpers/server-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
+const { InvalidParamError, ServerError, UnauthorizedError, MissingParamError } = require('../errors')
 const LoginRouter = require('./login-router')
 
 const makeSut = () => {
